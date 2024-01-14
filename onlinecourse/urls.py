5b83241d08994c43bc4df0c252dc06506ab8088d
nadeen-elsayed <dodynadeen@outlook.com>
2024-01-14 08:31:04 -0500

    Create a new exam result submission view

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-
 app_name = 'onlinecourse'
 urlpatterns = [
     # route is a string contains a URL pattern
@@ -17,8 +16,9 @@ urlpatterns = [
     # ex: /enroll/5/
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
-    # <HINT> Create a route for submit view
-
-    # <HINT> Create a route for show_exam_result view
+    # Create a route for submit view
+    path('<int:course_id>/submit/', views.submit, name="submit"),
+    # Create a route for show_exam_result view
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name="exam_result"),
 
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
